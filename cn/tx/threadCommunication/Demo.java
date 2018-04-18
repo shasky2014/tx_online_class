070fe22c754070de5eb82873cd15de01d7e791cc
shasky2014 <249398363@qq.com>
2018-04-19 00:04:55 +0800

    java学习记录

--- cn/tx/threadCommunication/Demo.java
+++ cn/tx/threadCommunication/Demo.java
@@ -3,8 +3,6 @@ package cn.tx.threadCommunication;
 public class Demo {
     public static void main(String[] args) {
         System.out.println("-----------------");
-        //
-        // git remote add origin https://github.com/shasky2014/tx_online_class.git
     }
 
 }
